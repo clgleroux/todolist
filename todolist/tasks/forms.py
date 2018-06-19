bdb08b9cc76f572cf548c343d4959c336a963c89
Clement Gauthier <clmt.gauthier@gmail.com>
2018-06-19 17:17:03 +0200

    [stable] first version

--- todolist/tasks/forms.py
+++ todolist/tasks/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from django.forms import ModelForm
+from django.forms.fields import TypedChoiceField
 from tasks.models import Task
 from django.utils.translation import gettext_lazy as _
 
@@ -39,23 +40,33 @@ class FilterForm(forms.Form):
 class FoundationSelectForm(forms.Select):
     template_name = 'tasks/select_choice_foundation.html'
 
+    def __init__(self, *args, instance_pk=None, **kwargs):
+        self.instance_pk = instance_pk
+        return super().__init__(*args, **kwargs)
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context['instance_pk'] = self.instance_pk
+        return context
+
 
 class UpdateForm(ModelForm):
     class Meta:
         model = Task
         fields = ['status']
-        labels = {
-            "status": ''
-        }
-        widgets = {
-            'status': FoundationSelectForm(),
-        }
 
     def __init__(self, *args, model_instance=None, **kwargs):
-        initial = {}
-        if model_instance is not None:
-            initial['status'] = model_instance.status
-        return super().__init__(*args, initial=initial, **kwargs)
+
+        result = super().__init__(*args, **kwargs)
+
+        self.fields['status'] = TypedChoiceField(
+            choices=Task.STATUS_CHOICES,
+            initial=model_instance and model_instance.status or None,
+            label='',
+            widget=FoundationSelectForm(
+                instance_pk=model_instance and model_instance.pk or None))
+
+        return result
 
     def save(self, pk):
         Task.objects.filter(pk=pk).update(**self.cleaned_data)
